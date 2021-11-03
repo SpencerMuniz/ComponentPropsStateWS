73cd4667a076dd9a0bd3264868f7ac3bb9ae6ec0
SpencerMuniz <sbottomley44@gmail.com>
2021-11-03 12:47:55 -0700

    created component folder with display name jsx. Finished display one name

---
+++ src/component/DisplayName.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class DisplayName extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            firstName: 'Indiana',
+            lastName: 'Jones'
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </div>
+        )
+    }
+}
+export default DisplayName
